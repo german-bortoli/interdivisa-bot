cf23ecf135218f6655586e4c52b03b6090c1a842
German Bortoli <german.bortoli@bairesdev.com>
2021-01-15 11:08:09 -0300

    Test heroku run

--- src/loaders/database.ts
+++ src/loaders/database.ts
@@ -3,7 +3,7 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 import config from '../config';
 
 export default async (): Promise<Connection> => {
-  const connectionOptions: PostgresConnectionOptions = {
+  let connectionOptions: PostgresConnectionOptions = {
     type: 'postgres',
     host: config.db.host,
     port: Number(config.db.port),
@@ -11,12 +11,17 @@ export default async (): Promise<Connection> => {
     password: config.db.password,
     database: config.db.database,
     synchronize: true,
-    ssl: {
-      rejectUnauthorized: false,
-    },
     logging: config.env !== 'production',
     entities: ['src/entities/*.entity.ts'],
   };
+
+  if (process.env.NODE_ENV === 'production') {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    connectionOptions['ssl'] = {
+      rejectUnauthorized: false,
+    };
+  }
 
   // create a connection using modified connection options
   return await createConnection(connectionOptions);
